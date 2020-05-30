f3f1ca33f45aca6295983cb7eed63957c96a8bcb
tyan <taeyeon0107@naver.com>
2020-05-30 19:29:15 +0900

    second_+CRUD code

--- secondproject/urls.py
+++ secondproject/urls.py
@@ -5,9 +5,15 @@ import portfolio.views
 from django.conf import settings
 from django.conf.urls.static import static
 
+import functioncrud.urls
+import functioncrud.views
+# import classcrud.urls
+# import classcrud.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', blog.views.home, name="home"),
+    path('', blog.views.home, name="home"),    #crud연습때문에 주석
+    # path('', functioncrud.views.welcome, name="welcome"),
 
     # path('blog/<int:blog_id>', blog.views.detail, name="detail"),
     # path('blog/new/', blog.views.new, name='new'),
@@ -17,5 +23,7 @@ urlpatterns = [
     # path('portfolio/', portfolio.views.portfolio, name="portfolio"),
     path('portfolio/', portfolio.views.portfolio, name='portfolio'),
     
+    path('funccrud/', include(functioncrud.urls)),
+    # path('classcrud/', include(classcrud.urls)),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
